f7b8841bf4722660d93e946c9540074c02166eb2
Meliani Mehdi <mehdi.hatachi@gmail.com>
2025-02-07 10:46:51 +0100

    added fragments.

--- app/src/main/java/com/app/lightenlife/MainActivity.kt
+++ app/src/main/java/com/app/lightenlife/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.app.lightenlife
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun goNext(v: View){
+        startActivity(Intent(this, Nav_activity::class.java))
+        finish()
     }
 }
--- app/src/main/java/com/app/lightenlife/Nav_activity.kt
+++ app/src/main/java/com/app/lightenlife/Nav_activity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.ismaeldivita.chipnavigation.ChipNavigationBar
 
 class Nav_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,29 @@ class Nav_activity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val bottomNavBar = findViewById<ChipNavigationBar>(R.id.bottom_nav_bar)
+
+        if (savedInstanceState == null) {
+            bottomNavBar.setItemSelected(R.id.nav_home, true)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment())
+                .commit()
+        }
+
+        bottomNavBar.setOnItemSelectedListener { id ->
+            val selectedFragment: Fragment = when (id) {
+                R.id.nav_home -> HomeFragment()
+                R.id.nav_time -> TimeFragment()
+                R.id.nav_test -> TestFragment()
+                R.id.nav_profile -> ProfileFragment()
+                else -> HomeFragment()
+            }
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, selectedFragment)
+                .commit()
         }
     }
 }
